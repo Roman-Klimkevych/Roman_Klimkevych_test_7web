f1191dae7dfd49236818ddbd4476298df49f75c0
Roman-Klimkevych <klimkevych@gmail.com>
2014-01-17 13:14:03 +0200

    App that shows list of text notes

--- djangotest/apps/notes/views.py
+++ djangotest/apps/notes/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.views.generic import ListView
+from notes.models import Notes
+
+class NotesView(ListView):
+	model = Notes
+	context_object_name = "notes"
+
+	def home(self, *args, **kwargs):
+		return HttpResponse()	
