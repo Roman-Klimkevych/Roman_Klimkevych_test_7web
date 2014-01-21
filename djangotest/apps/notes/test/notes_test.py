926bf6e1c8bba4f385d74d41a4cad4c17dcafd6c
Roman-Klimkevych <klimkevych@gmail.com>
2014-01-21 17:03:14 +0200

    test for adding text note form

--- djangotest/apps/notes/test/notes_test.py
+++ djangotest/apps/notes/test/notes_test.py
@@ -20,3 +20,19 @@ class MyTestCase(WebTest):
         
         # check text note in response
         assert 'Lorem ipsum dolor sit amet' in resp
+
+    def testNotesForm(self):
+        # check form.is_valid
+        resp = self.app.get(reverse('text_notes'))
+        form = resp.form
+        form['note'] = 'Note with more than 10 symbols'
+        res = form.submit()
+        assert res.status_int == 302
+        resp = self.app.get(reverse('text_notes'))
+        assert 'Your text note has been successfully added!!!' in resp
+
+        # check form.is_invalid
+        form['note'] = '123456789'
+        res = form.submit()
+        assert res.status_int == 200
+        assert 'Your note should contain at least 10 symbols!' in res
