d4ec767ba491ab1219cda075dbf53751a14efea9
Roman-Klimkevych <klimkevych@gmail.com>
2014-01-28 14:10:41 +0200

    Updated test

--- djangotest/apps/notes/test/notes_test.py
+++ djangotest/apps/notes/test/notes_test.py
@@ -6,67 +6,84 @@ from django.core.urlresolvers import reverse
 class MyTestCase(WebTest):
 
     """
-    Create test case for testing notes application
+    Create test case for testing notes application.
 
     First test case gets some initial data and application to be tested, 
-    then each function tests single task to be completed
+    then each function tests single task to be completed.
 
     """
 
-    """ Some initial data """
+    """ Some initial data. """
     fixtures = ['initial_data.json']
     
-    """ Application to be tested """
+    """ Application to be tested. """
     application = get_wsgi_application()
     app = TestApp(application)
     
 
     def test_notes(self):
         
-        """ Test that app shows list of text notes """
+        """ Test that app shows list of text notes. """
 
-        """ Check response status """
+        """ Check response status. """
         resp = self.app.get(reverse('text_notes'))
         self.assertEqual(resp.status_code, 200)
         
-        """ Check text note in response """
+        """ Check text note in response. """
         assert "Lorem ipsum dolor sit amet" in resp
 
     def test_notes_form(self):
         
-        """ Test the abilit to add new text note """
+        """ 
+        Test the ability to add new text note. 
 
-        """ Check that form.is_valid works as expected """
+        Updated to work with Ajax.
+
+        """
+
+        """ Check that form.is_valid works as expected. """
         resp = self.app.get(reverse('text_notes'))
         form = resp.form
-        form['note'] = "Note with more than 10 symbols"
-        res = form.submit()
-        self.assertEqual(res.status_code, 302)
-        resp = self.app.get(reverse('text_notes'))
-        assert "Your text note has been successfully added!!!" in resp
-
-        """ Check that form.is_invalid works as expected """
-        form['note'] = '123456789'
-        res = form.submit()
-        self.assertEqual(resp.status_code, 200)
+        res = self.app.post(reverse('ajax_notes'), 
+                            {   
+                                'csrfmiddlewaretoken':form['csrfmiddlewaretoken'].value,
+                                'note': 'Note with more than 10 symbols'
+                            })
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.content_type, "application/javascript")
+        assert "Your text note has been successfully added!!!" in res
+        
+        """ Check that form.is_invalid works as expected. """
+        res = self.app.post(reverse('ajax_notes'), 
+                            {   
+                                'csrfmiddlewaretoken':form['csrfmiddlewaretoken'].value,
+                                'note': '123456789'
+                            })
+        self.assertEqual(res.status_code, 200)
         assert "Your note should contain at least 10 symbols!" in res
 
     def test_form_field(self):
-        """ Test custom form field that returns notes changed to upper case"""
+        """ Test custom form field that returns notes changed to upper case."""
         resp = self.app.get(reverse('text_notes'))
         form = resp.form
-        form['note'] = "Note changed to upper case"
-        res = form.submit()
-        self.assertEqual(res.status_code, 302)
-        resp = self.app.get(reverse('text_notes'))
-        assert "NOTE CHANGED TO UPPER CASE" in resp
+        res = self.app.post(reverse('ajax_notes'), 
+                            {   
+                                'csrfmiddlewaretoken':form['csrfmiddlewaretoken'].value,
+                                'note': 'Note changed to upper case'
+                            })
+        self.assertEqual(res.status_code, 200)
+        assert "NOTE CHANGED TO UPPER CASE" in res
         
     def test_total_count(self):
-        """ Test total count of notes"""
+        """ Test total count of notes."""
         resp = self.app.get(reverse('text_notes'))
         form = resp.form
-        form['note'] = "Text note number 2"
-        res = form.submit()
-        self.assertEqual(res.status_code, 302)
-        resp = self.app.get(reverse('text_notes'))
-        assert "Text notes count: 2" in resp
+        res = self.app.post(reverse('ajax_notes'), 
+                            {   
+                                'csrfmiddlewaretoken':form['csrfmiddlewaretoken'].value,
+                                'note': 'Text note number 2'
+                            })
+        print res
+        self.assertEqual(res.status_code, 200)
+        assert '"count": 9' in res
+        
