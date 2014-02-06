a4df3cc5051078ea7ed0a10c96a29af2af222250
Roman-Klimkevych <klimkevych@gmail.com>
2014-02-06 15:56:17 +0200

    Test for Book model

--- djangotest/apps/notes/test/notes_test.py
+++ djangotest/apps/notes/test/notes_test.py
@@ -2,8 +2,10 @@ from django_webtest import WebTest
 from webtest import TestApp
 from django.core.wsgi import get_wsgi_application
 from django.core.urlresolvers import reverse
+from django.template import Template, Context
 # from djangotest import settings
 from django.conf import settings
+from notes.models import Notes, Book
 
 class MyTestCase(WebTest):
 
@@ -33,6 +35,23 @@ class MyTestCase(WebTest):
         
         """ Check text note in response. """
         assert "Lorem ipsum dolor sit amet" in resp
+
+    def test_custom_tag(self):
+
+        """ Test custom inclusion tag. """
+
+        """ Check response status. """
+        
+        resp = self.app.get(reverse('text_notes'))
+        self.assertEqual(resp.status_code, 200)
+        
+        """ Check custom template tag functionality in response. """
+        
+        note = Notes.objects.create(title='Note_1', text="Text Note")
+        t = Template('{% load custom_tags %}{% text_note id %}')
+        c = Context({"id": note.id})
+        resp = t.render(c)
+        assert 'Text Note' in resp
 
     def test_notes_form(self):
         
@@ -130,4 +149,16 @@ class MyTestCase(WebTest):
         resp = self.app.get(reverse('widget', args=['blue']))
         self.assertEqual(resp.status_code, 200)
         assert 'document.write' in resp
+
+    def test_book(self):
+        """ Test deleting book when last text note is deleted. """
+        book1 = Book(title="Book_1")
+        book1.save()
+        note1 = Notes(title="Note_1", text="text note")
+        note1.save()
+        note1.books.add(book1)
+        assert book1 in Book.objects.filter(notes=note1)
+        note1.delete()
+        assert book1 not in Book.objects.all()
+
 
